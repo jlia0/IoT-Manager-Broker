b447459cb94913351443b4fbbb34584be4ad81eb
JLiao98 <jian.liao1@ucalgary.ca>
2019-11-05 15:51:07 -0700

    added broker server query

--- app.js
+++ app.js
@@ -13,14 +13,6 @@ var app = express();
 
 
 
-// var query = "SELECT * FROM registry;"
-//
-// SQLQuery(query,function (status) {
-//
-// });
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
