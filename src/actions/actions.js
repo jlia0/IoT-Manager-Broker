bc3e3956f0552dbb957b7ba7cb98edd845310131
Artem Golovin <artgolovin96@gmail.com>
2019-11-27 10:06:56 -0700

    feat: save/update device to postgres

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -49,6 +49,15 @@ module.exports.saveSensorData = async body => {
       topic,
     });
 
+    // Insert new device if it doesn't exist
+    // otherwise, update the status to 'true'
+    const { rows } = await SQL(`select * from device where device_id = ${deviceId}`);
+    if (!rows.length) {
+      await SQL(`insert into device values (${deviceId}, true)`);
+    } else {
+      await SQL(`update device set device_status=true where device_id=${deviceId}`);
+    }
+
     // save it to the db
     await sensorData.save();
     console.log('data saved to mongo', body);
